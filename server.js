3654d2bf4ea4eea5ac159071379661d0e04e5fab
Hussam Khan <hussamkhan98@gmail.com>
2020-12-28 19:49:49 +0500

    done

--- server.js
+++ server.js
@@ -8,28 +8,49 @@ app.use(express.urlencoded({ extended: true }))
 
 app.get('/', async (req, res, next) => {
 	const { url } = req.query
+	var pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
+	if (url) {
+		// if (!pattern.test(url)) {
+		// 	return res.status(404).json({
+		// 		succes: false,
+		// 		message: 'Invalid URL format!',
+		// 		error: 'correct URL format:  https://www.google.com/',
+		// 	})
+		// }
+	} else {
+		return res.status(404).json({
+			succes: false,
+			message: 'Url required!',
+			error: `You need to send a url in the query parameter. e.g., https://${req.headers.host}?url=YOUR_URL`,
+		})
+	}
 	let _res
 	try {
 		_res = await axios.get(url)
 		res.status(200).send(_res.data)
 	} catch (error) {
-		return res
-			.status(500)
-			.json({ succes: false, message: 'Server error', error })
+		return res.status(500).json({ succes: false, message: 'Server error' })
 	}
 })
 
 app.post('/', async (req, res, next) => {
 	const { url } = req.query
 	const { data, headers } = req.body
+	let headrs = {}
+	if (headers) headrs = { ...headrs, headers }
+	if (!data)
+		return res.status(404).json({
+			succes: false,
+			message: 'data required!',
+			error: 'You need to send an object of data: {} in your request',
+		})
+
 	let _res
 	try {
-		_res = await axios.post(url, data, { ...headers })
+		_res = await axios.post(url, data, { ...headrs })
 		return res.status(200).send(_res.data)
 	} catch (error) {
-		return res
-			.status(500)
-			.json({ succes: false, message: 'Server error', error })
+		return res.status(500).json({ succes: false, message: 'Server error' })
 	}
 })
 app.put('/', async (req, res, next) => {
@@ -40,9 +61,7 @@ app.put('/', async (req, res, next) => {
 		_res = await axios.put(url, data, { ...headers })
 		return res.status(200).send(_res.data)
 	} catch (error) {
-		return res
-			.status(500)
-			.json({ succes: false, message: 'Server error', error })
+		return res.status(500).json({ succes: false, message: 'Server error' })
 	}
 })
 
@@ -54,9 +73,7 @@ app.delete('/', async (req, res, next) => {
 		_res = await axios.put(url)
 		return res.status(200).send(_res.data)
 	} catch (error) {
-		return res
-			.status(500)
-			.json({ succes: false, message: 'Server error', error })
+		return res.status(500).json({ succes: false, message: 'Server error' })
 	}
 })
 
