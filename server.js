34e1495e0fecd32f6bee60af8e2dc07d4b542564
Hussam Khan <hussamkhan98@gmail.com>
2022-07-21 21:53:26 +0500

    add cors

--- server.js
+++ server.js
@@ -1,32 +1,24 @@
 const express = require('express')
 const axios = require('axios')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.get('/', async (req, res, next) => {
 	const { url } = req.query
-	var pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
-	if (url) {
-		// if (!pattern.test(url)) {
-		// 	return res.status(404).json({
-		// 		succes: false,
-		// 		message: 'Invalid URL format!',
-		// 		error: 'correct URL format:  https://www.google.com/',
-		// 	})
-		// }
-	} else {
+	if (!url)
 		return res.status(404).json({
 			succes: false,
 			message: 'Url required!',
 			error: `You need to send a url in the query parameter. e.g., https://${req.headers.host}?url=YOUR_URL`,
 		})
-	}
 	let _res
 	try {
-		_res = await axios.get(url)
+		_res = await axios.get(url, { headers: { ...req.headers } })
 		res.status(200).send(_res.data)
 	} catch (error) {
 		return res.status(500).json({ succes: false, message: 'Server error' })
@@ -35,19 +27,9 @@ app.get('/', async (req, res, next) => {
 
 app.post('/', async (req, res, next) => {
 	const { url } = req.query
-	const { data, headers } = req.body
-	let headrs = {}
-	if (headers) headrs = { ...headrs, headers }
-	if (!data)
-		return res.status(404).json({
-			succes: false,
-			message: 'data required!',
-			error: 'You need to send an object of data: {} in your request',
-		})
-
 	let _res
 	try {
-		_res = await axios.post(url, data, { ...headrs })
+		_res = await axios.post(url, req.body, { headers: { ...req.headers } })
 		return res.status(200).send(_res.data)
 	} catch (error) {
 		return res.status(500).json({ succes: false, message: 'Server error' })
@@ -55,10 +37,9 @@ app.post('/', async (req, res, next) => {
 })
 app.put('/', async (req, res, next) => {
 	const { url } = req.query
-	const { data, headers } = req.body
 	let _res
 	try {
-		_res = await axios.put(url, data, { ...headers })
+		_res = await axios.put(url, req.body, { headers: { ...req.headers } })
 		return res.status(200).send(_res.data)
 	} catch (error) {
 		return res.status(500).json({ succes: false, message: 'Server error' })
@@ -67,10 +48,9 @@ app.put('/', async (req, res, next) => {
 
 app.delete('/', async (req, res, next) => {
 	const { url } = req.query
-	const { data, headers } = req.body
 	let _res
 	try {
-		_res = await axios.put(url)
+		_res = await axios.put(url, req.body, { headers: { ...req.headers } })
 		return res.status(200).send(_res.data)
 	} catch (error) {
 		return res.status(500).json({ succes: false, message: 'Server error' })
